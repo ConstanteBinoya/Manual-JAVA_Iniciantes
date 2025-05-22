3f21bfcce369787b3b8d343e425a5da59de68051
Constante Binoya <120677133+ConstanteBinoya@users.noreply.github.com>
2025-05-22 08:56:49 -0300

    Delete 1.estutura-básica-java directory

--- 1.estutura-básica-java/1.entendendoPacotes/src/br/edu/meuProjetoPacote/model/MinhaClasse.java
+++
@@ -1,8 +0,0 @@
-package br.edu.meuProjetoPacote.model;
-
-public class MinhaClasse {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Criar um pacote (package) em Java é essencial para organizar melhor o código");
-        System.out.println("Especialmente em projetos maiores.");
-    }
-}
--- 1.estutura-básica-java/3.definiçãoClasse/src/Aluno.java
+++
@@ -1,9 +0,0 @@
-public class Aluno {
-    String nome;
-    String curso;
-    int idade;
-
-    void apresentar() {
-        System.out.println("Olá, meu nome é " + nome + ", curso " + curso + " e tenho " + idade + " anos.");
-    }
-}
